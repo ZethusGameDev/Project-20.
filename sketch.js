6c96113b11dcd36952c1d93da189bc186afba643
ZethusGameDev <70686646+ZethusGameDev@users.noreply.github.com>
2020-12-08 12:34:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1500,400);
+
+  speed = random(55,90);
+  weight = random(400,1500);
+  
+  car = createSprite(100, 200, 50, 50);
+  car.velocityX = speed;
+
+  wall = createSprite(1200,200,60,height/2);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(wall.x - car.x < wall.width/2 + car.width/2) {
+    
+    car.velocityX = 0;
+    car.x = wall.x - 10;
+    car.collide(wall);
+
+    var deformation = 0.5 * weight * speed * speed/22500;
+
+    if(deformation > 180) {
+
+      car.shapeColor = "red";
+
+    }
+
+    if(deformation < 180 && deformation > 100) {
+
+      car.shapeColor = "yellow";
+
+    }
+    
+    if(deformation < 100) {
+
+      car.shapeColor = "green";
+
+    }
+  }
+
+
+  drawSprites();
+}
